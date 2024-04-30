0c75e024b2e3c479a08ec3954362a43eaa3022e3
jeathusan <141370299+Jeathu@users.noreply.github.com>
2024-04-30 22:34:11 +0200

    Delete gvm2 directory

--- gvm2/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:host=localhost;dbname=base', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-    exit;
-}
-
